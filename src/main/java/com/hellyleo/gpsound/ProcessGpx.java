17bc5c606ddb8b30ce4c7547145000dc84e0594d
lynerist <leonardo.albani@studenti.unimi.it>
2022-11-18 20:03:30 +0100

    gpx inizio

---
+++ src/main/java/com/hellyleo/gpsound/ProcessGpx.java
@@ -0,0 +1,29 @@
+package com.hellyleo.gpsound;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.EventListener;
+
+/**
+ *
+ * @author Utente
+ */
+public class ProcessGpx implements ActionListener{
+
+    final private Gpx gpx;
+    
+    public ProcessGpx(Gpx gpx) {
+        this.gpx = gpx;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            System.out.println(gpx.name);
+        } catch(Exception ec){
+            System.out.println(gpx);
+            System.out.println("Manca il File");
+        }
+    }
+    
+}
