265314724fabb3f5d63484e2c2aa7db021bab56d
lynerist <leonardo.albani@studenti.unimi.it>
2022-11-19 11:45:55 +0100

    load GPX

--- src/main/java/com/hellyleo/gpsound/Gpx.java
+++ src/main/java/com/hellyleo/gpsound/Gpx.java
@@ -4,18 +4,54 @@
  */
 package com.hellyleo.gpsound;
 
+import java.awt.Component;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+
 /**
  *
  * @author Utente
  */
+
 public class Gpx {
-    public String name;
+    
+    private String name;
+        
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void loadFile(Component view){
+        final JFileChooser fc = new JFileChooser();
+        fc.addChoosableFileFilter(new FileFilter(){
+            @Override
+            public boolean accept(File file){
+                return file.getName().endsWith(".gpx");
+            }
+            @Override
+            public String getDescription() {
+                return ".gpx";
+            }
+        });
+        fc.setAcceptAllFileFilterUsed(false);
+        
+        
+        int returnVal = fc.showDialog(view, "Load GPX");
+        
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            setName(file.getName());
+        }
+    }
 
     public Gpx(String name) {
         this.name = name;
     }
-   
-    public void setName(String name){
-        this.name = name;
-    }
+
+    
 }
