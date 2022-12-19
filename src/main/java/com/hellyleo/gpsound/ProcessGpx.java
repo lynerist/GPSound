ac8646eb198df31db435c5819c107fc3ee5d56b0
lynerist <leonardo.albani@studenti.unimi.it>
2022-12-19 13:10:28 +0100

    pitch down

--- src/main/java/com/hellyleo/gpsound/ProcessGpx.java
+++ src/main/java/com/hellyleo/gpsound/ProcessGpx.java
@@ -20,11 +20,14 @@ public class ProcessGpx implements ActionListener{
         if ( "".equals(model.getGpx().getName())) {
             System.out.println("Manca il File");
         } else {
-            for (int i = 0; i < 100; i++) {
+            //for (int i = 0; i < 100; i++) {
                 TrackPoint next = model.getGpx().getNext();
                 System.out.print(next+ " ");
                 System.out.println(deltaLongToStereo(next.getX()));
-            }
+                System.out.println(deltaLatToPitch(next.getY()));
+                System.out.println(deltaAltToAmp(next.getZ()));
+
+            //}
         } 
     }
     
@@ -37,26 +40,32 @@ public class ProcessGpx implements ActionListener{
     
     public float deltaAltToAmp(int delta){
         float maxDelta = 2000;
-        float amp = (float)(delta/maxDelta*2);
-        return amp>2?2:(amp<-2?-2:amp);       
+        float amp = 1+(float)(delta/(maxDelta*2));
+        return amp>2?2:(amp<0?0:amp);       
     }
     
     public float deltaLatToPitch(int delta){
         float[] maxDeltas = {1000,4000,6000,10000,50000}; //Sensibility values
         float maxDelta = maxDeltas[model.getSensibility()];
 
-        int numberOfOttaves = 6;
-        float mPerOttave = maxDelta/numberOfOttaves;
+        int numberOfOctaves = 6;
+        float metersPerOctave = maxDelta/numberOfOctaves;
         
-        // (delta/mPerOttave) -> number of octave jumps from startpitch 
-        // (delta%mPerOttave) reminder of the delta after the octave jumps
-        //delta%mPerOttave/mPerOttave percentage of octave to add 
+        // (delta/metersPerOctave) -> number of octave jumps from startpitch 
+        // (delta%metersPerOctave) reminder of the delta after the octave jumps
+        //delta%metersPerOctave/baseFrequence percentage of octave to add 
         int pitch = 0;
         if (delta>=0){
-            pitch = (int) ((delta/mPerOttave+1)*model.getStartPitch()+ (delta%mPerOttave/mPerOttave*model.getStartPitch()));        
+            
+            int baseFrequence = model.getStartPitch()*(int)Math.pow(2, (int)(delta/metersPerOctave));
+            pitch = (int) (baseFrequence + delta%metersPerOctave/metersPerOctave*baseFrequence);        
         }else{
-            // TODO NEGATIVE DELTA
-            // OCTAVE DIVISION
+            delta *= -1;
+            System.out.println("delta -> " + delta);
+            int baseFrequence = model.getStartPitch()/(int)Math.pow(2, (int)(delta/metersPerOctave));
+            System.out.println("baseFrequence -> " + baseFrequence);
+
+            pitch = baseFrequence>0?(int)(baseFrequence - delta%metersPerOctave/metersPerOctave*(baseFrequence/2)):20;
         }
         
         return pitch>20000?20000:(pitch<20?20:pitch);       
